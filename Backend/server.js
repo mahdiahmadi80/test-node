65c4c8fe0400b4ba25e9fddce19a31f093176a13
mahdi <127404389+mahdiahmadi80@users.noreply.github.com>
2024-10-31 19:05:09 +0330

    end project
    end

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 
 
 const app =express()
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173' 
+}));
 
 
 
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,46 +1,46 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const[data, setData]=useState([])
-  useEffect(()=>{
-    fetch("https://localhost:8081/city")
-    .then(res => res.json())
-    .then(data => setData(data))
-    .catch(err => console.error(err));
+  const [data, setData] = useState([]);
 
-  },[])
+  useEffect(() => {
+    fetch("http://localhost:8081/city") // اطمینان از استفاده از http
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok'); // بررسی وضعیت پاسخ
+        }
+        return res.json();
+      })
+      .then(data => setData(data))
+      .catch(err => console.error('Error fetching data:', err));
+  }, []);
 
   return (
     <div>
       <table>
-          <thead>
+        <thead>
+          <tr>
             <th>id</th>
             <th>name</th>
             <th>country</th>
             <th>district</th>
             <th>population</th>
-            <tbody>
-
-              {data.map((d,i)=>(
-                <tr key={i}>
-                  <td>{d.id}</td>
-                  <td>{d.name}</td>
-                  <td>{d.CountryCode}</td>
-                  <td>{d.District}</td>
-                  <td>{d.Population}</td>
-                </tr>
-
-
-                )
-              )}
-            </tbody>
-
-          </thead>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((d, i) => (
+            <tr key={i}>
+              <td>{d.ID}</td>
+              <td>{d.Name}</td>
+              <td>{d.CountryCode}</td>
+              <td>{d.District}</td>
+              <td>{d.Population}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
-  )
+  );
 }
 
-
-
-export default App
+export default App;
